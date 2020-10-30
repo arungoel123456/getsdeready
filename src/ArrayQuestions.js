d9f7cbcf5f66f7c7f2ca3e2dba21bd9a61d14123
arungoel123456 <goel.arungoel.arun@gmail.com>
2020-10-30 17:03:35 +0530

    Added more Questions

--- src/ArrayQuestions.js
+++ src/ArrayQuestions.js
@@ -80,9 +80,184 @@ const listOfQuestionsWithTopics = {
     'Sorting' : [
         {
             'K Closest Points to Origin': 'https://leetcode.com/problems/k-closest-points-to-origin',
+            'Maximum Difference':'https://www.geeksforgeeks.org/find-sum-maximum-difference-possible-subset-given-array/',
+            'Number of sextuplets' : 'https://www.geeksforgeeks.org/number-sextuplets-six-values-satisfy-equation/',
+            'Chocolate Distribution' : 'https://www.geeksforgeeks.org/chocolate-distribution-problem/' ,
 
         }
+    ],
+
+    'SortingII' : [
+        {
+            'Count of smaller numbers after self' : 'https://leetcode.com/problems/count-of-smaller-numbers-after-self/' ,
+            'Count of Range Sum' : 'https://leetcode.com/problems/count-of-range-sum/' ,
+            'Reverse pairs' : 'https://leetcode.com/problems/reverse-pairs/' ,
+            'Inversion count in an array':'https://www.interviewbit.com/problems/inversions/',
+        }
+    ],
+
+    'Two Pointer' : [
+        {
+            'Count of pairs with the given sum': 'https://www.geeksforgeeks.org/count-pairs-with-given-sum/',
+            'Minimum Size Subarray Sum' : 'https://leetcode.com/problems/minimum-size-subarray-sum/',
+            'Minimize the absolute difference' : 'https://www.geeksforgeeks.org/minimize-maxai-bj-ck-minai-bj-ck-three-different-sorted-arrays/',
+            'Smallest sequence with given Primes' : 'https://www.interviewbit.com/problems/smallest-sequence-with-given-primes/',
+            'Container With Most Water': 'https://leetcode.com/problems/container-with-most-water/',
+            'Counting Triangles' : 'https://www.interviewbit.com/problems/counting-triangles/'
+        }
+    ],
+
+    'Hashing' : [
+        {
+            'Subarray Sum Equals K' : 'https://leetcode.com/problems/subarray-sum-equals-k/',
+            'Largest Continuous Sequence Zero Sum' : 'https://www.interviewbit.com/problems/largest-continuous-sequence-zero-sum/',
+            'Longest Consecutive Sequence' : 'https://leetcode.com/problems/longest-consecutive-sequence/',
+            'Equal': 'https://www.interviewbit.com/problems/equal/',
+            'Anagrams' : 'https://www.interviewbit.com/problems/anagrams/',
+            'Valid Sudoku' : 'https://leetcode.com/problems/valid-sudoku/',
+            'Set Matrix Zeros' : 'https://www.interviewbit.com/problems/set-matrix-zeros/',
+            'Find pairs in array whose sum is already present' : 'https://www.geeksforgeeks.org/find-pairs-in-array-whose-sums-already-exist-in-array/',
+
+        }
+    ],
+
+    'Hashing II' : [
+        {
+            'Palindrome Pairs' : 'https://leetcode.com/problems/palindrome-pairs/',
+            'Window String' : 'https://leetcode.com/problems/minimum-window-substring/',
+            'Longest Substring Without Repeat' : 'https://www.interviewbit.com/problems/longest-substring-without-repeat/',
+            'Points on same line' : 'https://leetcode.com/problems/max-points-on-a-line/' ,
+            'Count Right Triangles' : 'https://www.spoj.com/problems/RIGHTTRI/',
+            'Diffk II' : 'https://www.interviewbit.com/problems/diffk-ii/',
+            'Grid Illumination' : 'https://leetcode.com/problems/grid-illumination/',
+        }
+    ],
+
+    'Strings' : [
+        {
+            'Longest Common Prefix' : 'https://www.interviewbit.com/problems/longest-common-prefix/',
+            'Compare Version Numbers': 'https://www.interviewbit.com/problems/compare-version-numbers/',
+            'Reverse the String' : 'https://www.interviewbit.com/problems/reverse-the-string/',
+            'Implement StrStr': 'https://leetcode.com/problems/implement-strstr/',
+            'Integer To Roman' : 'https://www.interviewbit.com/problems/integer-to-roman/',
+            'Roman To Integer' : 'https://www.interviewbit.com/problems/roman-to-integer/',
+            'Pretty Json': 'https://www.interviewbit.com/problems/pretty-json/',
+        }
+    ],
+
+    'Stacks' : [
+        {
+            'Evaluate Expression' : 'https://www.interviewbit.com/problems/evaluate-expression/',
+            'Min Stack' : 'https://leetcode.com/problems/min-stack/',
+            'Largest Rectangle in Histogram' : 'https://leetcode.com/problems/largest-rectangle-in-histogram/',
+            'Maximum Rectangle' : 'https://leetcode.com/problems/largest-rectangle-in-histogram/',
+            'Trapping Rain Water' : 'https://leetcode.com/problems/trapping-rain-water/',
+            'Nearest Smaller Element' : 'https://www.interviewbit.com/problems/nearest-smaller-element/',
+        }
+    ],
+
+    'Queues' : [
+        {
+            'Sum of min and max' : 'https://www.geeksforgeeks.org/sum-minimum-maximum-elements-subarrays-size-k/',
+            'N integers containing only 1, 2 & 3' : 'https://practice.geeksforgeeks.org/problems/numbers-containing-1-2-and-3/0',
+            'Gas Station' : 'https://leetcode.com/problems/gas-station/',
+            'Sliding Window Maximum' : 'https://leetcode.com/problems/sliding-window-maximum/' ,
+            'First non-repeating character' : 'https://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/',
+        }
+    ],
+
+    'Linked List' : [
+        {
+            'Remove Nth Node From End of List' : 'https://leetcode.com/problems/remove-nth-node-from-end-of-list/',
+            'List Cycle' : 'https://www.interviewbit.com/problems/list-cycle/',
+            'Merge Two Sorted Lists' : 'https://leetcode.com/problems/merge-two-sorted-lists/',
+            'Reverse Link List II' : 'https://leetcode.com/problems/reverse-linked-list-ii/',
+            'Palindrome List' : 'https://leetcode.com/problems/palindrome-linked-list/',
+            'Middle element of linked list' : 'https://leetcode.com/problems/middle-of-the-linked-list/',
+
+        }
+    ],
+
+    'Linked List II' : [
+        {
+            'Merge Two Sorted Lists' : 'https://www.interviewbit.com/problems/merge-two-sorted-lists/',
+            'Flatten a Multilevel Doubly Linked List' : 'https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/',
+            'Clone a Linked List' : 'https://www.geeksforgeeks.org/a-linked-list-with-next-and-arbit-pointer/',
+            'Intersection of Linked Lists' : 'https://leetcode.com/problems/intersection-of-two-linked-lists/',
+            'Remove Duplicates from Sorted List II' : 'https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/',
+            'Remove Duplicates from Sorted List' : 'https://leetcode.com/problems/remove-duplicates-from-sorted-list/',
+            'Longest Palindromic List': 'https://www.geeksforgeeks.org/length-longest-palindrome-list-linked-list-using-o1-extra-space/',
+
+
+        }
+    ],
+
+    'Trees' : [
+        {
+            'Inorder Traversal': 'https://www.interviewbit.com/problems/inorder-traversal/',
+            'Postorder Traversal' : 'https://www.interviewbit.com/problems/postorder-traversal/',
+            'Binary Tree From Inorder And Postorder': 'https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/',
+            'Balanced Binary Tree': 'https://leetcode.com/problems/balanced-binary-tree/',
+            'Preorder Traversal': 'https://www.interviewbit.com/problems/preorder-traversal/',
+            'ZigZag Level Order Traversal BT' : 'https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/',
+            'Construct Binary Tree from Preorder and Inorder Traversal': 'https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/',
+
+        }
+    ],
+
+    'Trees II' : [
+        {
+            'Vertical Order traversal' : 'https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/',
+            'Binary Tree Right Side View': 'https://leetcode.com/problems/binary-tree-right-side-view/',
+            '2-Sum Binary Tree' : 'https://www.interviewbit.com/problems/2sum-binary-tree/' ,
+            'TOP VIEW' : 'https://www.geeksforgeeks.org/print-nodes-top-view-binary-tree/' ,
+            'ZigZag Level Order Traversal BT' : 'https://www.interviewbit.com/problems/zigzag-level-order-traversal-bt/',
+        }
+    ],
+
+    'Trees III & BST I' : [
+        {
+            'Least Common Ancestor' : 'https://www.interviewbit.com/problems/least-common-ancestor/',
+            'Invert the Binary Tree' : 'https://www.interviewbit.com/problems/invert-the-binary-tree/',
+            'Valid Binary Search Tree' : 'https://leetcode.com/problems/validate-binary-search-tree/',
+            'Path Sum' : 'https://www.interviewbit.com/problems/path-sum/' ,
+            'Symmetric Binary Tree' : 'https://www.interviewbit.com/problems/symmetric-binary-tree/',
+        }
+    ],
+
+    'BST' : [
+        {
+            'Recover Binary Search Tree' : 'https://leetcode.com/problems/recover-binary-search-tree/',
+            'Sorted Array To Balanced BST' : 'https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/',
+            'BST Iterator' : 'https://www.interviewbit.com/problems/bst-iterator/' ,
+            'Left view of binary tree' : 'https://www.geeksforgeeks.org/print-left-view-binary-tree/',
+            'Sum Root to Leaf Numbers' : 'https://leetcode.com/problems/sum-root-to-leaf-numbers/',
+            'Symmetric Binary Tree' : 'https://www.interviewbit.com/problems/symmetric-binary-tree/' ,
+            'Identical Binary Trees' : 'https://www.interviewbit.com/problems/identical-binary-trees/' ,
+            'Root to Leaf Paths With Sum' : 'https://leetcode.com/problems/sum-root-to-leaf-numbers/' ,
+
+        }
+    ],
+
+    'BST II' : [
+        {
+            'Leaf nodes from Preorder of a Binary Search Tree' : 'https://www.geeksforgeeks.org/leaf-nodes-preorder-binary-search-tree/',
+            'Distance between Nodes of BST' : 'https://leetcode.com/problems/minimum-distance-between-bst-nodes/' ,
+            'Boundary Traversal Of Binary Tree' : 'https://www.geeksforgeeks.org/boundary-traversal-of-binary-tree/',
+            '2-Sum Binary Tree' : 'https://www.interviewbit.com/problems/2sum-binary-tree/' ,
+            'Binary Tree Paths' : 'https://leetcode.com/problems/binary-tree-paths/' ,
+            'All Nodes Distance K in Binary Tree' : 'https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/',
+
+        }
+    ],
+
+    'Heap' : [
+        {
+            
+        }
     ]
+
+
 }
 
 export {listOfQuestionsWithTopics}
